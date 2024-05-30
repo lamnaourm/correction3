c0599165781cafdd5051c8ad248516fc1315b086
lamnaourm <mohammed.lamnaour@gmail.com>
2024-05-30 09:55:39 +0100

    q5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Composant2 from './components/Composant2';
+import Composant3 from './components/Composant3';
 
 function App() {
 
@@ -16,7 +19,12 @@ function App() {
   },[])
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/q2' element={<Composant2 salaries={salaries} />} />
+          <Route path='/q3' element={<Composant3 salaries={salaries} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
